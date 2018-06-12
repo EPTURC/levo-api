34435d531237aefedbd99bff72a3c2b406df16dd
Sarah <sarah@marvin.local>
2018-06-12 10:40:10 -0300

    Show vehicle locations on vehicles index

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,7 @@
 class Vehicle < ApplicationRecord
     has_many :locations
+
+    def as_json(options={})
+      super(:only => [:id, :company_id], :include => { locations: { only: [:latitude, :longitude, :created_at] } })
+    end
 end
