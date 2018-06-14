2804f384dbcab9a219fcba0d6787bbf5bb56e2cd
Sarah <sarah@marvin.local>
2018-06-14 14:25:32 -0300

    Add api docs

--- app/controllers/api/v1/drivers_controller.rb
+++ app/controllers/api/v1/drivers_controller.rb
@@ -1,18 +1,18 @@
 class Api::V1::DriversController < ApplicationController
   before_action :set_driver, only: [:show, :update, :destroy]
-
+  api :GET, "drivers", "Show all drivers"
   # GET /api/v1/drivers
   def index
     @drivers = Driver.all
 
     render json: @drivers
   end
-
+  api :GET, "drivers/:id", "Show a driver details"
   # GET /api/v1/drivers/1
   def show
     render json: @driver
   end
-
+  api :POST, "drivers", "Create a driver"
   # POST /api/v1/drivers
   def create
     @driver = Driver.new(driver_params)
@@ -23,7 +23,7 @@ class Api::V1::DriversController < ApplicationController
       render json: @driver.errors, status: :unprocessable_entity
     end
   end
-
+  api :PUT, "drivers/:id", "Edit a driver"
   # PATCH/PUT /api/v1/drivers/1
   def update
     if @driver.update(driver_params)
@@ -32,7 +32,7 @@ class Api::V1::DriversController < ApplicationController
       render json: @driver.errors, status: :unprocessable_entity
     end
   end
-
+  api :DELETE, "drivers/:id", "Delete a driver"
   # DELETE /api/v1/drivers/1
   def destroy
     @driver.destroy
--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -2,15 +2,18 @@ class Api::V1::LocationsController < ApplicationController
   before_action :set_vehicle
   before_action :set_location, only: %i[show]
 
+  api :GET, "vehicles/:id_vehicle/locations", "Show all locations for the vehicle"
   def index
     @locations = @vehicle.locations
     render json: @locations.as_json(only: [:latitude, :longitude, :created_at])
   end
 
+  api :GET, "vehicles/:id_vehicle/locations/:id", "Show a location details for the vehicle"
   def show
     render json: @location.as_json(only: [:latitude, :longitude, :created_at])
   end
 
+  api :POST, "vehicles/:id_vehicle/locations", "Create a new location for the vehicle"
   def create
     @location = Location.new(location_params)
     @location.vehicle = @vehicle
