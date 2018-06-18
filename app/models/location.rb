21ddfd5d49036decc8704783433ec976c91d3955
Sarah <sarah@marvin.local>
2018-06-18 20:25:47 -0300

    Add vehicle status

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,4 +4,9 @@ class Location < ApplicationRecord
   def as_json(options={})
     super(:only => [:latitude, :longitude, :created_at])
   end
+
+  def minutes_since_updated
+    (Time.current - self.created_at) / 1.minute
+  end
+
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,11 +3,26 @@ class Vehicle < ApplicationRecord
     belongs_to :driver
 
     def as_json(options={})
-      super(:only => [:id, :company_id, :driver_id], :include => { last_location: { only: [:latitude, :longitude, :created_at] } } )
+      super(:only => [:id, :company_id, :driver_id], :include => { last_location: { only: [:latitude, :longitude, :created_at] }}, :methods => [:status] )
     end
 
     def last_location
       self.locations.last
+    end
+
+    def is_stopped
+      #to-do: calculate distance between last locations
+      false
+    end
+
+    def status
+      if (self.last_location.minutes_since_updated > 1)
+        "offline" 
+      elsif self.is_stopped
+        "stopped" 
+      else
+        "moving" 
+      end
     end
 
 end
