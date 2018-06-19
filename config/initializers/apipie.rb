5460d0784177905ca1798407769d6f8ad10d1956
Sarah <sarah@marvin.local>
2018-06-19 14:46:10 -0300

    Fix docs

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -2,6 +2,7 @@ Apipie.configure do |config|
   config.app_name                = "LevoApi"
   config.api_base_url            = "/api/v1/"
   config.doc_base_url            = "/docs"
+  config.translate = false
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/v1/*.rb"
 end
