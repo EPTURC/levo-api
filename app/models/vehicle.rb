709e2e51d9343b70a55bfef2af958f8c7f4205b8
Tyrone Macedo Damasceno <tyronedamasceno@gmail.com>
2018-06-20 13:07:40 -0300

    Revert "Add is_stopped method to Vehicle"

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -11,32 +11,8 @@ class Vehicle < ApplicationRecord
     end
 
     def is_stopped
-      last = self.locations.last(3)
-      v1 = last[0]
-      v2 = last[1]
-      v3 = last[2]
-      if (v2.nil? && v3.nil?)
-        true
-      elsif (v3.nil?)
-        if (dist(v1, v2) < 1)
-          true
-        else
-          false
-        end
-      else
-        if (dist(v1, v2) < 1 && dist(v1, v3) < 1 && dist(v2, v3) < 1)
-          true
-        else
-          false
-        end
+      #to-do: calculate distance between last locations
       false
-      end
-    end
-
-    def dist(v1, v2)
-      dla = ((v1.latitude.to_d - v2.latitude.to_d).abs) * 1852
-      dlo = ((v1.longitude.to_d - v2.longitude.to_d).abs) * 1852
-      Math.sqrt(dla ** 2 + dlo ** 2)
     end
 
     def status
