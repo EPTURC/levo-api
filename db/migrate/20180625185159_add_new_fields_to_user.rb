b6632512013fbe96acd93af00c7a9eee1b470efe
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-06-27 07:00:47 -0300

    migrations lost in last commit

---
+++ db/migrate/20180625185159_add_new_fields_to_user.rb
@@ -0,0 +1,8 @@
+class AddNewFieldsToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :phone, :string
+    add_column :users, :rg, :string
+    add_column :users, :cpf, :string
+    add_column :users, :adress, :string
+  end
+end
