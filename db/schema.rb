2402e6a932f4948479cd9d716bc62b66dffb6033
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-06-28 07:20:34 -0300

    Comitting migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_25_180959) do
+ActiveRecord::Schema.define(version: 2018_06_25_162156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,13 +52,6 @@ ActiveRecord::Schema.define(version: 2018_06_25_180959) do
     t.index ["vehicle_id"], name: "index_locations_on_vehicle_id"
   end
 
-  create_table "occurences", force: :cascade do |t|
-    t.string "title"
-    t.string "descrie"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "occurrences", force: :cascade do |t|
     t.string "type"
     t.string "location"
@@ -67,7 +60,7 @@ ActiveRecord::Schema.define(version: 2018_06_25_180959) do
     t.datetime "updated_at", null: false
     t.bigint "driver_id"
     t.bigint "itinerary_id"
-    t.boolean "solved", default: false
+    t.boolean "solved"
     t.index ["driver_id"], name: "index_occurrences_on_driver_id"
     t.index ["itinerary_id"], name: "index_occurrences_on_itinerary_id"
   end
@@ -92,9 +85,6 @@ ActiveRecord::Schema.define(version: 2018_06_25_180959) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "company_id"
-    t.string "license_plate"
-    t.string "model"
-    t.string "color"
   end
 
   add_foreign_key "drivers", "users"
