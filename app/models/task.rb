34d89e0b6e66f92df7bc01e7247d9dc6f8f5825f
Sarah <sarah@marvin.local>
2018-06-28 18:44:27 -0300

    Fix task type

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,8 @@
 class Task < ApplicationRecord
     self.inheritance_column = nil
 
-    TYPE_OPTIONS = ['Entrega', 'Coleta']
-    validates_inclusion_of :type, :in => TYPE_OPTIONS, message: "Invalid type. Valid options are: 'Entrega' and 'Coleta'"
+
+
 
     def as_json(options={})
         super(:only => [:id, :object, :responsible_name, :type, :local])
