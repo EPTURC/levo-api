530f410b7a2114f3001e3ecbf71d3f200e9fea92
Victor Hugo de Oliveira <victor-olliveira@live.com>
2018-06-30 05:14:18 -0300

    Add authenticate_user! method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,32 @@
 class ApplicationController < ActionController::API
   require 'json_web_token'
+
+  protected
+  def authenticate_user!
+    if !retrieve_payload || !JsonWebToken.valid_payload(retrieve_payload.first)
+      return invalid_token
+    end
+
+    load_current_user!
+    invalid_token unless @current_user
+  end
+
+  def invalid_token
+    render json: { error: 'Invalid authentication token' }, status: :unauthorized
+  end
+
+  private
+  def retrieve_payload
+    auth_header = request.headers['token']
+
+    token = auth_header.split(' ').last
+
+    JsonWebToken.decode(token)
+  rescue
+    nil
+  end
+
+  def load_current_user!
+    @current_user = User.find_by(id: retrieve_payload[0]['user_id'])
+  end
 end
